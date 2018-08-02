bc9a67fbeff4989c919a5781f3cdef50b3a89c75
louisa wruck <louisa.wruck@gmail.com>
2018-08-02 18:37:35 +0200

    refactor: Books correctly sorted
    Headings missing

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -9,40 +9,36 @@ class BookShelf extends React.Component {
     if (this.props.readStatus.map(subarray => subarray.map(book=> book)) !== []) {
   return (
       <div key='123'>
-        {this.props.sortBooks.map(category=> (
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">{}</h2>      {/* Fix this!*/}
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {console.log(this.props.readStatus.map(subarray => subarray.map(book=> book)))}
-
-                {this.props.readStatus.map(subarray => subarray.map( book => (
-                    <li key={book.id}>
-                      {/*console.log(book)*/}
-                      <div className="book">
-                        <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}>
+        {this.props.readStatus.map(subarray =>(
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">BOOKS</h2>      {/* Fix this!*/}
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {subarray.map( book => (
+                            <li key={book.id}>
+                              {/*console.log(book)*/}
+                              <div className="book">
+                                <div className="book-top">
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}>
+                              </div>
+                                  <div className="book-shelf-changer">
+                                    <select>
+                                      <option value="move" disabled>Move to...</option>
+                                      <option value="currentlyReading">Currently Reading</option>
+                                      <option value="wantToRead">Want to Read</option>
+                                      <option value="read">Read</option>
+                                      <option value="none" onClick={() => this.props.deleteBook(book)}>None</option>
+                                    </select>
+                                  </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.authors}</div>
+                              </div>
+                            </li> )) }
+                        </ol>
                       </div>
-                          <div className="book-shelf-changer">
-                            <select>
-                              <option value="move" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none" onClick={() => this.props.deleteBook(book)}>None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors}</div>
-                      </div>
-                    </li>
-                )))}
-
-              </ol>
-            </div>
-          </div>
-    ))}
+                    </div>
+                  ))}
           </div>  // Closing the main div
 )}
 else {return (<div>Error</div>)
@@ -50,3 +46,5 @@ else {return (<div>Error</div>)
 }
 }
   export default BookShelf
+
+  /*console.log(this.props.readStatus.map(subarray => subarray.map(book=> book)))*/
