433e5e672067ef82f670a7e9e10b5ffe7bcdff0d
louisa wruck <louisa.wruck@gmail.com>
2018-08-03 17:15:28 +0200

    refactor: search - displays books correctly
    bug: no space accepted in search, delete all characters => page breaks
    down

--- src/SearchBooksList.js
+++ src/SearchBooksList.js
@@ -8,15 +8,17 @@ class SearchBooksList extends React.Component {
   }
   componentDidUpdate(prevProps, prevState){
     this.setState((newState) =>
-{        if(newState !== prevState) {
-            console.log(newState.query + prevState.query)
-              //  BooksAPI.search(this.state.query).then((bookSearch) => {
-              //    this.setState({bookSearch})
-        }//)
+    { if (newState !== prevState) {
+      console.log(newState.query + prevState.query)
+        if (newState.query !== prevState.query) {
+               BooksAPI.search(this.state.query).then((bookSearch) => {
+                  this.setState({bookSearch})
+        })}
+         }
         else{console.log('This went wrong')}
-      }//}
-    )
-  }
+      })
+}
+
   // Search bar - write input in state.query
     onSearch = (e) => {
       e.preventDefault()
@@ -40,6 +42,35 @@ class SearchBooksList extends React.Component {
                   ref = {input => this.search = input}
                   />
           </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{}</h2>      {/* Fix this!*/}
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {this.state.bookSearch.map( book => (
+                            <li key={book.id}>
+                              {/*console.log(book)*/}
+                              <div className="book">
+                                <div className="book-top">
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}>
+                              </div>
+                                  <div className="book-shelf-changer">
+                                    <select onClick={this.props.handleClick}>
+                                      <option value="move" disabled>Move to...</option>
+                                      <option value="currentlyReading">Currently Reading</option>
+                                      <option value="wantToRead">Want to Read</option>
+                                      <option value="read">Read</option>
+                                      <option value="none">None</option>
+                                    </select>
+                                  </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.authors}</div>
+                              </div>
+                            </li> )) }
+                        </ol>
+                      </div>
+                    </div>
+
         </div>
     )
   }
